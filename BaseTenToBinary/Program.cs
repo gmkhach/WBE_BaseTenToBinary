0fc2557d845de54f176bae3c56a1b24e59886e5a
Gabriel Khachatryan <gmkhach@gmail.com>
2018-11-02 23:10:53 -0400

    Add project files.

---
+++ BaseTenToBinary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BaseTenToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
