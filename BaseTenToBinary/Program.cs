4a0a986711fbd10a5484841e3adf1d729254d812
Gabriel Khachatryan <gmkhach@gmail.com>
2018-11-02 23:24:36 -0400

    brute-force method

--- BaseTenToBinary/Program.cs
+++ BaseTenToBinary/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace BaseTenToBinary
 {
@@ -6,7 +8,44 @@ namespace BaseTenToBinary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            do
+            {
+                try
+                {
+                    Console.Write("\nEnter an integer betwen 1 and 100\n\n>>> ");
+                    int input = int.Parse(Console.ReadLine());
+                    if (input < 1 || input > 100)
+                    {
+                        Console.Clear();
+                        throw new Exception("Invalid Entry!");
+                    }
+                    Console.WriteLine("\nBinary equivalent: " + BaseTenToBinary(input));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\n" + ex.Message);
+                }
+                Console.Write("\nPress Enter to try another number...");
+                Console.ReadLine();
+                Console.Clear();
+            } while (true);
+        }
+
+        static int BaseTenToBinary(int baseTen)
+        {
+            string binary = "1";
+            Stack<string> myStack = new Stack<string>();
+
+            while(baseTen != 1)
+            {
+                myStack.Push(Convert.ToString(baseTen % 2));
+                baseTen = baseTen / 2;
+            }
+            while (myStack.Count != 0)
+            {
+                binary += myStack.Pop();
+            }
+                return int.Parse(binary);
         }
     }
 }
